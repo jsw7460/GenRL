abef24c676267965dca528e3397e317f5a975294
jsw7460 <jsw7460@gmail.com>
2023-08-24 12:50:19 +0900

    Fix [Diffusion prediction]

--- genrl/policies/low/diffusion/arch/mlpdiffusion.py
+++ genrl/policies/low/diffusion/arch/mlpdiffusion.py
@@ -7,14 +7,12 @@ from genrl.utils.jax_utils.general import create_mlp
 
 
 class MLPDiffusionNN(nn.Module):
-    x_dim: int
-    y_dim: int
     embed_dim: int
     hidden_dim: int
     output_dim: int
     dropout: float
-    max_step: int = 1024
     activation_fn: Callable
+    total_denoise_steps: int
 
     emb_x = None
     emb_y = None
@@ -38,7 +36,7 @@ class MLPDiffusionNN(nn.Module):
             dropout=self.dropout
         )
         self.emb_t = nn.Sequential([
-            nn.Embed(self.max_step),
+            nn.Embed(num_embeddings=self.total_denoise_steps, features=self.embed_dim),
             jnp.sin,
             nn.Dense(self.embed_dim)
         ])
@@ -67,29 +65,30 @@ class MLPDiffusionNN(nn.Module):
     def __call__(self, *args, **kwargs):
         return self.forward(*args, **kwargs)
 
-    def forward(self, x: jnp.ndarray, y: jnp.ndarray, t: jnp.ndarray):
+    def forward(self, x: jnp.ndarray, y: jnp.ndarray, t: jnp.ndarray, deterministic: bool = False) -> jnp.ndarray:
         """
         :param x: observation
         :param y: action
         :param t: denoising step
+        :param deterministic
         :return:
         """
-        emb_x = self.emb_x(x)
-        emb_y = self.emb_y(y)
+        emb_x = self.emb_x(x, deterministic=deterministic)
+        emb_y = self.emb_y(y, deterministic=deterministic)
         emb_t = self.emb_t(t)
 
         in1 = jnp.concatenate((emb_x, emb_y, emb_t), axis=-1)
-        out1 = self.out1(in1)
+        out1 = self.out1(in1, deterministic=deterministic)
 
         in2 = jnp.concatenate((out1 / 1.414, emb_y, emb_t), axis=-1)
-        out2 = self.out2(in2)
+        out2 = self.out2(in2, deterministic=deterministic)
         out2 = out2 + out1 / 1.414
 
         in3 = jnp.concatenate((out2 / 1.414, emb_y, emb_t), axis=-1)
-        out3 = self.out3(in3)
+        out3 = self.out3(in3, deterministic=deterministic)
         out3 = out3 + out2 / 1.414
 
-        in4 = jnp.concatenate((out3 / 1.414, y, t), axis=-1)
-        out4 = self.out4(in4)
+        in4 = jnp.concatenate((out3 / 1.414, y, emb_t), axis=-1)
+        out4 = self.out4(in4, deterministic=deterministic)
 
         return out4
--- genrl/policies/low/diffusion/models.py
+++ genrl/policies/low/diffusion/models.py
@@ -4,7 +4,6 @@ from typing import Dict, Tuple
 import jax
 from jax import numpy as jnp
 
-from genrl.utils import AttrDict
 from genrl.policies.low.diffusion.ddpm_schedule import ddpm_linear_schedule
 from genrl.policies.low.base import BaseLowPolicy
 from genrl.policies.low.diffusion.arch.mlpdiffusion import MLPDiffusionNN  # Todo: Change to transformer
@@ -20,43 +19,70 @@ class DiffusionLowPolicy(BaseLowPolicy):
     def __init__(self, seed: int, env: GymEnv, cfg: Dict, init_build_model: bool = True):
         super(DiffusionLowPolicy, self).__init__(seed=seed, env=env, cfg=cfg, init_build_model=init_build_model)
 
-        self.n_denoise = cfg["n_denoise"]
+        self.total_denoise_steps = cfg["total_denoise_steps"]
         self.noise_dim = cfg["noise_dim"]
         self.ddpm_schedule = ddpm_linear_schedule(**self.cfg["ddpm_schedule"])
 
     def build(self):
-        nn = MLPDiffusionNN(**self.cfg["nn_cfg"])
-        obs = jnp.array([self.env.observation_space.sample()])
+        super(DiffusionLowPolicy, self).build()
+        nn_class = MLPDiffusionNN
+
+        nn = nn_class(**self.cfg["nn_cfg"])
+        obs = jnp.array([self.env.observation_space["observation"].sample()])
         act = jnp.array([self.env.action_space.sample()])
-        time = jnp.array([0])
-        self.rng, rngs = get_basic_rngs(self.rng)
+        time = jnp.array([0], dtype="i4")
 
         tx = self.optimizer_class(learning_rate=self.cfg["lr"], **self.cfg["optimizer_kwargs"])
-        self.policy = Model.create(
+        self.rng, rngs = get_basic_rngs(self.rng)
+
+        self.policy_nn = Model.create(
             apply_fn=nn.apply,
-            params=nn.init(self.rng, obs, act, time),
+            params=nn.init(self.rng, obs, act, time, False),
             tx=tx
         )
 
+    @staticmethod
+    @jax.jit
+    def policy_forward(
+        policy: Model,
+        observations: jnp.ndarray,
+        y_t: jnp.ndarray,
+        denoise_step: jnp.ndarray,
+        deterministic: bool = True
+    ) -> jnp.ndarray:
+        """
+        reverse process of diffusion
+        :return y_{t-1}
+        """
+        noise_pred = policy.apply_fn(
+            policy.params,
+            x=observations,
+            y=y_t,
+            t=denoise_step,
+            deterministic=deterministic
+        )
+
+        return noise_pred
+
     def update(self, replay_data: GenRLBufferSample) -> Dict:
         self.rng, _ = jax.random.split(self.rng)
+
         new_policy, info = self.update_diffusion(
             rng=self.rng,
-            policy=self.policy,
+            policy=self.policy_nn,
             observations=replay_data.observations,
             actions=replay_data.actions,
             masks=replay_data.masks,
 
             ddpm_schedule=self.ddpm_schedule,
             noise_dim=self.noise_dim,
-            n_denoise=self.n_denoise
+            total_denoise_steps=self.total_denoise_steps
         )
-
-        print("Run!!" * 999)
-
+        self.policy_nn = new_policy
+        return info
 
     @staticmethod
-    @partial(jax.jit, static_argnames=("noise_dime", "n_denoise"))
+    @partial(jax.jit, static_argnames=("noise_dim", "total_denoise_steps"))
     def update_diffusion(
         rng: PRNGKey,
         policy: Model,
@@ -64,31 +90,35 @@ class DiffusionLowPolicy(BaseLowPolicy):
         actions: jnp.ndarray,   # [b, l, d]
         masks: jnp.ndarray, # [b, l]
 
-        ddpm_schedule: AttrDict,
+        ddpm_schedule: Dict,
         noise_dim: int,
-        n_denoise: int,
+        total_denoise_steps: int,
     ) -> Tuple[Model, Dict]:
         batch_size = observations.shape[0]
+        subseq_len = observations.shape[1]
+
         broadcast_shape = (batch_size,) + (1,) * (actions.ndim - 1)
 
         # sample noise
         noise = jax.random.normal(rng, shape=(*actions.shape[:-1], noise_dim))
 
         # add noise to clean target actions
-        _ts = jax.random.randint(rng, shape=(batch_size,), minval=1, maxval=n_denoise)
-        sqrtab = ddpm_schedule.sqrtab[_ts].reshape(broadcast_shape)
-        sqrtmab = ddpm_schedule.sqrtmab[_ts].reshape(broadcast_shape)
+        _ts = jax.random.randint(rng, shape=(batch_size,), minval=1, maxval=total_denoise_steps)
+
+        sqrtab = ddpm_schedule["sqrtab"][_ts].reshape(broadcast_shape)
+        sqrtmab = ddpm_schedule["sqrtmab"][_ts].reshape(broadcast_shape)
 
         y_t = sqrtab * actions + sqrtmab * noise
 
         # use diffusion model to predict noise
         def _loss(params: Params) -> Tuple[jnp.ndarray, Dict]:
             noise_pred = policy.apply_fn(
-                {"params": params},
+                params,
                 x=observations,
                 y=y_t,
-                t=_ts
+                t=jnp.repeat(_ts[..., jnp.newaxis], repeats=subseq_len, axis=-1)
             )
+
             mse_loss = jnp.mean(jnp.sum((noise_pred - noise) ** 2, axis=-1) * masks)
 
             _info = {
@@ -102,3 +132,30 @@ class DiffusionLowPolicy(BaseLowPolicy):
         new_policy = policy.apply_gradients(grads=grads)
 
         return new_policy, info
+
+    def _predict(self, observations: jnp.ndarray, deterministic: bool = True) -> Tuple[jnp.ndarray, Dict]:
+        broadcast_shape = observations.shape[: -1]
+
+        # sample initial noise, y_T ~ Normal(0, 1)
+        y_t = jax.random.normal(self.rng, shape=(*observations.shape[: -1], self.noise_dim))
+        # denoising chain
+        for t in range(self.total_denoise_steps, 0, -1):
+            denoise_steps = t + jnp.zeros(shape=broadcast_shape, dtype="i4")
+            z = jax.random.normal(self.rng, shape=(*observations.shape[: -1], self.noise_dim)) if t > 0 else 0
+
+            eps = DiffusionLowPolicy.policy_forward(
+                policy=self.policy_nn,
+                observations=observations,
+                y_t=y_t,
+                denoise_step=denoise_steps,
+                deterministic=True
+            )
+
+            oneover_sqrta = self.ddpm_schedule["oneover_sqrta"]
+            ma_over_sqrtmab_inv = self.ddpm_schedule["ma_over_sqrtmab_inv"]
+            sqrt_beta_t = self.ddpm_schedule["sqrt_beta_t"]
+
+            y_t = (oneover_sqrta[t] * (y_t - ma_over_sqrtmab_inv[t] * eps)) + (sqrt_beta_t[t] * z)
+
+        self.rng, _ = jax.random.split(self.rng)
+        return y_t
