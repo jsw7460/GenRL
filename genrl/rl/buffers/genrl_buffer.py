57e795cad4f7e5aef773287af6150aa33208724b
jsw7460 <jsw7460@gmail.com>
2023-08-24 12:50:54 +0900

    Add [Dataset Split]

--- genrl/rl/buffers/genrl_buffer.py
+++ genrl/rl/buffers/genrl_buffer.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, Optional, Union, Any
+from typing import Callable, Optional, Union, Any, List
 
 import numpy as np
 from jax.tree_util import tree_map
@@ -11,26 +11,55 @@ from minari.dataset.minari_dataset import EpisodeData
 from minari.dataset.minari_storage import MinariStorage, PathLike
 
 
-class GenRLBuffer(MinariDataset):
+class GenRLDataset(MinariDataset):
 
     def __init__(
         self,
         data: Union[MinariStorage, PathLike],
         episode_indices: Optional[np.ndarray] = None,
+        postprocess_fn: Optional[Callable[[dict], Any]] = None,
     ):
-        super(GenRLBuffer, self).__init__(data=data, episode_indices=episode_indices)
+        super(GenRLDataset, self).__init__(data=data, episode_indices=episode_indices)
+        self.postprocess_fn = postprocess_fn
+
+    @classmethod
+    def split_dataset(cls, buffer: GenRLDataset, sizes: List[int], seed: Optional[int] = None):
+        """Split a MinariDataset in multiple datasets.
+
+            Args:
+                buffer (GenRLDataset): the GenRLDataset to split
+                sizes (List[int]): sizes of the resulting datasets
+                seed (Optiona[int]): random seed
+
+            Returns:
+                datasets (List[MinariDataset]): resulting list of datasets
+            """
+        if sum(sizes) > buffer.total_episodes:
+            raise ValueError(
+                "Incompatible arguments: the sum of sizes exceeds ",
+                f"the number of episodes in the dataset ({buffer.total_episodes})",
+            )
+        generator = np.random.default_rng(seed=seed)
+        indices = generator.permutation(buffer.episode_indices)
+        out_datasets = []
+        start_idx = 0
+        for length in sizes:
+            end_idx = start_idx + length
+            slice_dataset = cls(buffer.spec.data_path, indices[start_idx:end_idx], buffer.postprocess_fn)
+            out_datasets.append(slice_dataset)
+            start_idx = end_idx
+
+        return out_datasets
 
     def sample_subtrajectories(
         self,
         n_episodes: int,
         subseq_len: int,
-        postprocess_fn: Optional[Callable[[dict], Any]],
         allow_replace: bool = False
     ) -> GenRLBufferSample:
         indices = self._generator.choice(self.episode_indices, size=n_episodes, replace=allow_replace)
-        dict_data = self._data.apply(postprocess_fn, indices)
+        dict_data = self._data.apply(self.postprocess_fn, indices)
         episodes = [EpisodeData(**data) for data in dict_data]
-
         out = []
 
         for ep in episodes:
