b9e67361baab58d27897b34065a185bb6f2c39ca
jsw7460 <jsw7460@gmail.com>
2023-08-24 12:49:50 +0900

    Fix [Diffusion DDPM schedule]

--- genrl/policies/low/diffusion/ddpm_schedule.py
+++ genrl/policies/low/diffusion/ddpm_schedule.py
@@ -1,10 +1,13 @@
 from jax import numpy as jnp
+from typing import Dict
 
-from genrl.utils import AttrDict
 
+def ddpm_linear_schedule(beta1: float, beta2: float, total_denoise_steps: int) -> Dict:
+    beta_t = (beta2 - beta1) \
+             * jnp.arange(-1, total_denoise_steps, dtype=jnp.float32) \
+             / (total_denoise_steps - 1) \
+             + beta1
 
-def ddpm_linear_schedule(beta1: float, beta2: float, total_denoise_step: int) -> AttrDict:
-    beta_t = (beta2 - beta1) * jnp.arange(-1, total_denoise_step, dtype=jnp.float32) / (total_denoise_step - 1) + beta1
     beta_t = beta_t.at[0].set(beta1)
 
     sqrt_beta_t = jnp.sqrt(beta_t)
@@ -17,14 +20,15 @@ def ddpm_linear_schedule(beta1: float, beta2: float, total_denoise_step: int) ->
 
     sqrtmab = jnp.sqrt(1 - alpha_bar_t)
     mab_over_sqrtmab_inv = (1 - alpha_bar_t) / sqrtmab
+    ma_over_sqrtmab_inv = (1 - alpha_t) / sqrtmab
 
-    # shape = (total_denoise_step, )
-    return AttrDict(
-        alpha_t=alpha_t,
-        oneover_sqrta=oneover_sqrta,
-        sqrt_beta_t=sqrt_beta_t,
-        alpha_bar_t=alpha_bar_t,
-        sqrtab=sqrtab,
-        sqrtmab=sqrtmab,
-        mab_over_sqrtmab_inv=mab_over_sqrtmab_inv
-    )
+    return {
+        "alpha_t": alpha_t,
+        "oneover_sqrta": oneover_sqrta,
+        "sqrt_beta_t": sqrt_beta_t,
+        "alpha_bar_t": alpha_bar_t,
+        "sqrtab": sqrtab,
+        "sqrtmab": sqrtmab,
+        "mab_over_sqrtmab_inv": mab_over_sqrtmab_inv,
+        "ma_over_sqrtmab_inv": ma_over_sqrtmab_inv
+    }
