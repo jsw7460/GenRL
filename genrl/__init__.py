0301fbd98c5c3e55e639b17f638022b8eba6e396
jsw7460 <jsw7460@gmail.com>
2023-08-27 15:49:05 +0900

    Fix verbose

--- genrl/__init__.py
+++ genrl/__init__.py
@@ -1,5 +1,7 @@
 import logging
+import os
 import warnings
 
+os.environ["JAX_IGNORE_TPU_DEVICES"] = "1"
 warnings.simplefilter("ignore", UserWarning)
 logging.getLogger('jax._src.lib.xla_bridge').addFilter(lambda _: False)
