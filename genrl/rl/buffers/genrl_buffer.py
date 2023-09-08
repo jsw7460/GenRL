dada8c21fc0853f05c9a9e830d03c68e0c711c25
jsw7460 <jsw7460@gmail.com>
2023-09-08 12:28:50 +0900

    Fix [Subtraj sampling]

--- genrl/rl/buffers/genrl_buffer.py
+++ genrl/rl/buffers/genrl_buffer.py
@@ -1,13 +1,29 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import Callable, Optional, Union, Any, List, Iterable, Tuple, Type
 
 import numpy as np
-from jax.tree_util import tree_map
 from minari import MinariDataset
 from minari.dataset.minari_storage import PathLike, MinariStorage
 
-from vlg.rl.buffers.type_aliases import VLGBufferSample, VLGEpisodeData
+from genrl.rl.buffers.type_aliases import GenRLBufferSample, GenRLEpisodeData
+
+
+@dataclass(frozen=True)
+class SubTrajectoryData:
+    """
+        Contains the datasets data for subtrajectories.
+    """
+    total_timesteps: int
+    observations: np.ndarray
+    actions: np.ndarray
+    rewards: np.ndarray
+    terminations: np.ndarray
+    truncations: np.ndarray
+    episode_ids: int
+    next_observations: np.ndarray
+    timestep_ranges: np.ndarray
 
 
 class GenRLDataset(MinariDataset):
@@ -24,11 +40,11 @@ class GenRLDataset(MinariDataset):
         self._generator = np.random.default_rng(seed)
         self.preprocess_obs = preprocess_obs
 
-        self.episodes = None  # type: Union[None, List[VLGEpisodeData, ...]]
-        self.__use_memory_cache = False
+        self.episodes = None  # type: Union[None, List[GenRLEpisodeData, ...]]
+        self._use_memory_cache = False
 
     def filter_episodes(
-        self, condition: Callable[[VLGEpisodeData], bool]
+        self, condition: Callable[[GenRLEpisodeData], bool]
     ) -> GenRLDataset:
         """Filter the dataset episodes with a condition.
 
@@ -45,7 +61,7 @@ class GenRLDataset(MinariDataset):
         """
 
         def dict_to_episode_data_condition(episode: dict) -> bool:
-            return condition(VLGEpisodeData(**episode))
+            return condition(GenRLEpisodeData(**episode))
 
         mask = self._data.apply(
             dict_to_episode_data_condition, episode_indices=self._episode_indices
@@ -91,59 +107,61 @@ class GenRLDataset(MinariDataset):
 
         return tuple(out_datasets)
 
-    def sample_episodes(self, n_episodes: int) -> Iterable[VLGEpisodeData]:
+    def sample_episodes(self, n_episodes: int) -> Iterable[GenRLEpisodeData]:
         """Sample n number of episodes from the dataset.
 
         Args:
             n_episodes (Optional[int], optional): number of episodes to sample.
         """
-        if self.__use_memory_cache:
+        if self._use_memory_cache:
             return self._generator.choice(self.episodes, size=n_episodes, replace=True)
 
         else:
             indices = self._generator.choice(self.episode_indices, size=n_episodes, replace=True)
             episodes = self._data.get_episodes(indices)
-            return list(map(lambda data: VLGEpisodeData(**data), episodes))
+            return list(map(lambda data: GenRLEpisodeData(**data), episodes))
 
     def cache_data(self):
-        """
-            Reading the hdf5 every time a trajectory is sampled is not good for speed and the lifespan of the disk.
-            When this method is called, it stores the episode data inside the class in the form of a list.
-            This increases memory usage but provides a faster sampling speed.
+        """Reading the hdf5 every time a trajectory is sampled is not good for speed and the lifespan of the disk.
+        When this method is called, it stores the episode data inside the class in the form of a list.
+        This increases memory usage but provides a faster sampling speed.
         """
         self.episodes = self.sample_episodes(n_episodes=len(self))
-        self.__use_memory_cache = True
+        self._use_memory_cache = True
 
     def sample_subtrajectories(
         self,
         n_episodes: int,
         subseq_len: int,
-    ) -> VLGBufferSample:
+    ) -> GenRLBufferSample:
 
         episodes = self.sample_episodes(n_episodes)
 
         _thresholds = np.array([ep.total_timesteps - 1 for ep in episodes])
         start_idxs = self._generator.integers(0, _thresholds)
 
-        out = []
+        ep_sample = next(iter(episodes))
 
-        for ep, start_idx in zip(episodes, start_idxs):
+        # Pre-allocate numpy arrays
+        observations_batch = np.empty((n_episodes, subseq_len, ep_sample.observations.shape[-1]))
+        next_observations_batch = np.empty_like(observations_batch)
+        actions_batch = np.empty((n_episodes, subseq_len, ep_sample.actions.shape[-1]))
+        rewards_batch = np.empty((n_episodes, subseq_len))
+        terminations_batch = np.empty((n_episodes, subseq_len))
+        truncations_batch = np.empty((n_episodes, subseq_len))
+        masks_batch = np.empty((n_episodes, subseq_len))
+        episode_ids_batch = np.empty((n_episodes,), dtype=int)
+        timestep_ranges_batch = np.empty((n_episodes, subseq_len), dtype=int)
+
+        for i, (ep, start_idx) in enumerate(zip(episodes, start_idxs)):
             end_idx = start_idx + subseq_len
-            timesteps_range = np.arange(start_idx, end_idx)
 
-            ep_obs = self.preprocess_obs(ep.observations)
-
-            # obs = ep.observations["observation"]
-            # obs = obs[start_idx: end_idx, ...]
-
-            obs = ep_obs[start_idx: end_idx, ...]
+            obs = ep.observations[start_idx: end_idx, ...]
             act = ep.actions[start_idx: end_idx, ...]
-
-            # next_obs = obs[start_idx + 1: end_idx + 1, ...]
-
-            next_obs = ep_obs[start_idx + 1: end_idx + 1, ...]
+            next_obs = ep.observations[start_idx + 1: end_idx + 1, ...]
             rew = ep.rewards[start_idx: end_idx, ...]
             terminations = ep.terminations[start_idx: end_idx, ...]
+            truncations = ep.truncations[start_idx: end_idx, ...]
 
             pad_size = subseq_len - len(act)
             obs_padding = np.zeros((subseq_len - len(obs), obs.shape[-1]))
@@ -151,27 +169,29 @@ class GenRLDataset(MinariDataset):
             act_padding = np.zeros((pad_size, act.shape[-1]))
             rew_padding = np.zeros((pad_size,))
             terminations_padding = np.zeros((pad_size,))
+            truncations_padding = np.zeros((pad_size,))
 
-            observations = np.concatenate((obs, obs_padding), axis=0)
-            actions = np.concatenate((act, act_padding), axis=0)
-            next_observations = np.concatenate((next_obs, next_obs_padding.copy()), axis=0)
-            rewards = np.concatenate((rew, rew_padding), axis=0)
-            terminations = np.concatenate((terminations, terminations_padding), axis=0)
-            masks = np.concatenate((np.ones((subseq_len - pad_size,)), np.zeros((pad_size,))), axis=0)
+            observations_batch[i] = np.concatenate((obs, obs_padding), axis=0)
+            next_observations_batch[i] = np.concatenate((next_obs, next_obs_padding.copy()), axis=0)
+            actions_batch[i] = np.concatenate((act, act_padding), axis=0)
+            rewards_batch[i] = np.concatenate((rew, rew_padding), axis=0)
+            terminations_batch[i] = np.concatenate((terminations, terminations_padding), axis=0)
+            truncations_batch[i] = np.concatenate((truncations, truncations_padding), axis=0)
+            masks_batch[i] = np.concatenate((np.ones((subseq_len - pad_size)), np.zeros((pad_size,))), axis=0)
+            episode_ids_batch[i] = ep.id
+            timestep_ranges_batch[i] = np.arange(start_idx, end_idx)
 
-            subtraj_data = {
-                "episode_id": ep.id,
-                "timesteps_range": timesteps_range,
-                "subseq_len": subseq_len,
-                "observations": observations,
-                "actions": actions,
-                "next_observations": next_observations,
-                "rewards": rewards,
-                "terminations": terminations,
-                "masks": masks,
-            }
-            out.append(subtraj_data)
-
-        out = tree_map(lambda *arr: np.stack(arr, axis=0), *out)
-        out = VLGBufferSample(**out)
+        data = {
+            "subseq_len": subseq_len,
+            "observations": observations_batch,
+            "next_observations": next_observations_batch,
+            "actions": actions_batch,
+            "rewards": rewards_batch,
+            "terminations": terminations_batch,
+            "truncations": truncations_batch,
+            "masks": masks_batch,
+            "episode_ids": episode_ids_batch,
+            "timestep_ranges": timestep_ranges_batch,
+        }
+        out = GenRLBufferSample(**data)
         return out
